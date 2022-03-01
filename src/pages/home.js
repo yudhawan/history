8609e0fcf9d7a41aaefe57765df7ff3e9f0869dc
Yasha <yudhaashter@gmail.com>
2022-03-01 14:19:55 +0700

    ci

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,7 +3,7 @@ import Banner from '../banner.jpg'
 function Home() {
   return (
     <div>
-      <div className={`flex flex-col items-center justify-center  bg-transparent backdrop-blur-sm`} >
+      <div className={`flex flex-col items-center justify-center`} >
         <div className='font-semibold text-xl text-white underline decoration-sky'>WELCOME TO GASING</div>
       </div>
     </div>
